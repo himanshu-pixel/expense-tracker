05d0649e8faec21dd37ce5de8c9d6d74e8102d34
Himanshu Kushwaha <kushwahahimanshu26@gmail.com>
2023-02-13 23:52:46 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Expenses from "./components/expenses/Expenses";
 import NewExpense from "./components/newExpense/NewExpense";
-import Expenses from "./components/expenses/expenses";
 const DUMMY_EXPENSES = [
   { key: "e1", date: new Date(2020, 7, 14), item: "Panner", cost: 45 },
   { key: "e2", date: new Date(2021, 2, 28), item: "Milk", cost: 78 },
