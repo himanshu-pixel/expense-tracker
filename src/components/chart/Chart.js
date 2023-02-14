810f067456e5247838db92e38f377b1ad0b82cd2
Himanshu Kushwaha <kushwahahimanshu26@gmail.com>
2023-02-14 19:47:29 +0530

    intial commit

---
+++ src/components/chart/Chart.js
@@ -0,0 +1,17 @@
+import ChartBar from "./ChartBar";
+import "./Chart.css";
+const Chart = (props) => {
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoint) => (
+        <ChartBar
+          key={dataPoint.label}
+          value={dataPoint.value}
+          maxValue={null}
+          lable={dataPoint.label}
+        />
+      ))}
+    </div>
+  );
+};
+export default Chart;
